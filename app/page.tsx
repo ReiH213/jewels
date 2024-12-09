3536a4001523d84655bf15dcae5226f3d223f3e2
Rei <rei.haxhiraj@xyzteknoloji.com>
2024-12-09 08:18:14 +0300

    Fixed error

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import JewelsCarousel from "@/components/JewelsCarousel";
-
-export default async function Home() {
+export async function getServerSideProps() {
   let result: JewelItem[] = [];
   try {
     const response = await fetch("https://jewelsback.onrender.com/items", {
@@ -9,16 +8,38 @@ export default async function Home() {
         "Content-Type": "application/json",
       },
     });
+
     if (response.ok) {
       result = await response.json();
-    } else {
-      console.error(
-        `Failed to fetch items: ${response.status} ${response.statusText}`
-      );
     }
   } catch (error) {
     console.error("Fetch error:", error);
   }
+
+  return {
+    props: { items: result },
+  };
+}
+
+export default async function Home({ items }: { items: JewelItem[] }) {
+  // let result: JewelItem[] = [];
+  // try {
+  //   const response = await fetch("https://jewelsback.onrender.com/items", {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   });
+  //   if (response.ok) {
+  //     result = await response.json();
+  //   } else {
+  //     console.error(
+  //       `Failed to fetch items: ${response.status} ${response.statusText}`
+  //     );
+  //   }
+  // } catch (error) {
+  //   console.error("Fetch error:", error);
+  // }
   return (
     <main className="w-full flex flex-col items-center h-screen overflow-x-hidden ">
       <section className="flex flex-col h-64 gap-y-10 items-center w-screen justify-center colorGradient animatedGradient">
@@ -32,7 +53,7 @@ export default async function Home() {
       <h1 className="mt-10 text-4xl font-extrabold text-gray-400">
         Take a look at our most popular products
       </h1>
-      <JewelsCarousel items={result} />
+      <JewelsCarousel items={items} />
     </main>
   );
 }
